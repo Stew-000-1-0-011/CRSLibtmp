749274aebe3549c503e8d1cf6d89e762931f6aa9
Stew-000-1-0-011 <stew00010011@gmail.com>
2023-03-07 01:57:23 +0900

    forward_likeの実装が面倒なので削除

--- Ros1/utility.hpp
+++ Ros1/utility.hpp
@@ -8,29 +8,6 @@
 
 namespace CRSLib
 {
-	// 引用: cppref forward_like
-	template<class T, class U>
-	constexpr auto&& forward_like(U&& x) noexcept
-	{
-		constexpr bool is_adding_const = std::is_const_v<std::remove_reference_t<T>>;
-		if constexpr (std::is_lvalue_reference_v<T&&>) {
-			if constexpr (is_adding_const) {
-				return std::as_const(x);
-			}
-			else {
-				return static_cast<U&>(x);
-			}
-		}
-		else {
-			if constexpr (is_adding_const) {
-				return std::move(std::as_const(x));
-			}
-			else {
-				return std::move(x);
-			}
-		}
-	}
-
 	namespace Implement
 	{
 		template<class T_>
