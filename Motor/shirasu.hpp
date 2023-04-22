53c7f27719ee3f5200e912222a5326345e02b635
Stew00010011@gmail.com <Stew00010011@gmail.com>
2023-04-22 15:00:36 +0900

    送るべきはModeではなくCmd

--- Motor/shirasu.hpp
+++ Motor/shirasu.hpp
@@ -3,6 +3,7 @@
 
 #pragma once
 
+#include <iostream>  // debug
 #include <utility>
 #include <numbers>
 
@@ -15,12 +16,13 @@ namespace CRSLib::Motor
 {
 	enum class ShirasuState : u8
 	{
-		disable,
-		duty,
-		current,
-		velocity,
-		position,
-		homing
+		shutdown,
+		recover,
+		home,
+		get_status,
+		recover_current,
+		recover_velocity,
+		recover_position
 	};
 
 	template<Can::MainPC::pillarbox Pillarbox, reporter Reporter>
@@ -29,7 +31,7 @@ namespace CRSLib::Motor
 		Pillarbox command;
 		Pillarbox target;
 		Reporter reporter;
-		ShirasuState state{ShirasuState::disable};
+		ShirasuState state{ShirasuState::shutdown};
 		float current_current{0};
 		float current_speed{0};
 		float current_position{0};
@@ -43,53 +45,53 @@ namespace CRSLib::Motor
 
 		void stop()
 		{
-			change_state(ShirasuState::velocity);
+			change_state(ShirasuState::recover_velocity);
 			update_speed(0);
 		}
 
 		void turn_off()
 		{
-			change_state(ShirasuState::disable);
+			change_state(ShirasuState::shutdown);
 		}
 
 		void update_current(const float x)
 		{
-			change_state(ShirasuState::current);
+			change_state(ShirasuState::recover_current);
 			send_target(x);
 			current_current = x;
 		}
 
 		void add_current(const float x)
 		{
-			change_state(ShirasuState::current);
+			change_state(ShirasuState::recover_current);
 			current_current += x;
 			send_target(current_current);
 		}
 
 		void update_speed(const float x)
 		{
-			change_state(ShirasuState::velocity);
+			change_state(ShirasuState::recover_velocity);
 			send_target(x * std::numbers::pi_v<float> * 2 / 60);
 			current_speed = x;
 		}
 
 		void add_speed(const float x)
 		{
-			change_state(ShirasuState::velocity);
+			change_state(ShirasuState::recover_velocity);
 			current_speed += x;
 			send_target(current_speed * std::numbers::pi_v<float> * 2 / 60);
 		}
 
 		void update_position(const float x)
 		{
-			change_state(ShirasuState::position);
+			change_state(ShirasuState::recover_position);
 			send_target(x / 360 * 2 * std::numbers::pi_v<float>);
 			current_position = x;
 		}
 
 		void add_position(const float x)
 		{
-			change_state(ShirasuState::position);
+			change_state(ShirasuState::recover_position);
 			current_position += x;
 			send_target(current_position / 360 * 2 * std::numbers::pi_v<float>);
 		}
@@ -102,16 +104,16 @@ namespace CRSLib::Motor
 			}
 			else
 			{
-				if(s == ShirasuState::disable || s == ShirasuState::homing)
+				if(s == ShirasuState::shutdown || s == ShirasuState::home)
 				{
 					current_current = 0;
 					current_speed = 0;
-					if(s == ShirasuState::homing)
+					if(s == ShirasuState::home)
 					{
 						current_position = 0;
 					}
 				}
-				else if(state != ShirasuState::disable) reporter("This version of Motor::Shirasu may behave unexpectedly in this state transition.");
+				else if(state != ShirasuState::shutdown) reporter("This version of Motor::Shirasu may behave unexpectedly in this state transition.");
 
 				state = s;				
 				command.post(Can::DataField{.buffer=static_cast<byte>(to_underlying(s)), .dlc=1});
@@ -121,6 +123,7 @@ namespace CRSLib::Motor
 		private:
 		void send_target(const float x)
 		{
+			std::cout << "shirasu: send_target " << x << std::endl;  // debug
 			Can::DataField data{.dlc=4};
 			std::memcpy(data.buffer, &x, 4);
 			std::swap(data.buffer[0], data.buffer[3]);
